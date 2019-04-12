ed694b80a154e5564fb88b7b1b9b3d99858045e6
cuxy6705 <cuxy0000@gmail.com>
2019-04-12 18:07:31 +0800

    第二頁完

---
+++ beeALL.js
@@ -0,0 +1,193 @@
+let data = {
+    logo: 'LOGO',
+    menu: [{
+        name: '首頁'
+    }, {
+        name: '熱門'
+    }, {
+        name: '社會'
+    }, {
+        name: '娛樂'
+    }, {
+        name: '生活'
+    }, {
+        name: '國際'
+    }, {
+        name: '健康'
+    }, {
+        name: '旅遊'
+    }, ],
+    news: [{
+        index: 1,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22'
+    }, {
+        index: 2,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22'
+    }, {
+        index: 3,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22'
+    }, {
+        index: 4,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22'
+    }],
+    mininews: [{
+        index: 1,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22',
+        mininews: '亞科新聞'
+    }, {
+        index: 2,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22',
+        mininews: '亞科新聞'
+    }, {
+        index: 3,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22',
+        mininews: '亞科新聞'
+    }, {
+        index: 4,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22',
+        mininews: '亞科新聞'
+    }, {
+        index: 5,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22',
+        mininews: '亞科新聞'
+    }, {
+        index: 6,
+        minititle: '社會',
+        title: '新聞標題',
+        time: '2019/04/01 22:22',
+        mininews: '亞科新聞'
+    }],
+    rightnews: [{
+        index: 1,
+        title: '六輕氣爆報告出爐／管線洩漏上午就發現 雲林縣長怒',
+        miniti: '亞科新聞'
+    }, {
+        index: 2,
+        title: '日本 Janpara 店家 Google Pixel 3 超特價只要新台幣14,000元',
+        miniti: '三立新聞'
+    }, {
+        index: 3,
+        title: '英若硬脫歐 聯合國盤點全球最大贏家與輸家',
+        miniti: '公視新聞'
+    }, {
+        index: 4,
+        title: '《玩很大》黑隊隊長換他！　隔4年合體小鬼…網嗨翻：《角頭》團聚',
+        miniti: '東森新聞'
+    }, {
+        index: 5,
+        title: '標普500指數終止連八漲 全球經濟和貿易摩擦又蒙陰霾',
+        miniti: '民視新聞'
+    }, {
+        index: 6,
+        title: '英若硬脫歐 聯合國盤點全球最大贏家與輸家',
+        miniti: '財經新聞'
+    }, {
+        index: 7,
+        title: '大甲媽遶彰化警察被打傷 方仰寧嗆黑道「相堵ㄟ丟！」',
+        miniti: '八七新聞'
+    }, {
+        index: 8,
+        title: '神魔之塔x《聖鬥士星矢》合作確認 與雅典娜及眾多聖鬥士並肩作戰！',
+        miniti: '想不到新聞'
+    }, {
+        index: 9,
+        title: '關於史上首張黑洞照片，這些是你需要的科普資料',
+        miniti: '高雄新聞'
+    }, {
+        index: 10,
+        title: '慟!!!!中午不知道要吃什麼',
+        miniti: '台灣新聞'
+    }],
+    footer: [{
+        index: 1,
+        link: "相關連結"
+    }, {
+        index: 2,
+        link: "相關連結"
+    }, {
+        index: 3,
+        link: "相關連結"
+    }, {
+        index: 4,
+        link: "相關連結"
+    }, {
+        index: 5,
+        link: "相關連結"
+    }, {
+        index: 6,
+        link: "相關連結"
+    }, {
+        index: 7,
+        link: "相關連結"
+    }, {
+        index: 8,
+        link: "相關連結"
+    }],
+    textnews: {
+        title: "維基解密創辦人艾山吉 被拖出倫敦大使館",
+        time: "2019/04/12 04:10",
+        view: "94878",
+        text: "揭露40萬筆美國機密檔案的揭密網站「維基解密」創辦人艾山吉，在滯留厄瓜多駐英大使館近7年後，遭倫敦警方強行拖出大使館，警方表示逮捕行動是應美國當局的引渡要求。美國司法部證實，已向英國提出引渡要求，一旦艾山吉被引渡到美國，入侵電腦罪名成立，刑期為5年。英國首相府稱未向厄國政府遊說下逐客令，而英國法院昨稍後裁決，艾山吉當年棄保躲入厄使館有罪。 美司部法 已要求引渡 昨天上午，厄國基於艾山吉違反政治庇護條件，戲劇性翻臉撤銷庇護，結束他在使館內6年9個月又24天的「作客」生涯。7、8名彪形大漢把他又拉又抬挪出大使館，久未公開露臉的艾山吉蓄著滿臉落腮鬍，在被上銬強行帶上車前，他對著大使館外的群眾高喊「英國一定要抵抗(美國)到底！」表達抗議。他被捕時手上拿著美國知名小說兼劇作家戈爾·維達爾的作品《國家安全狀態史》。 史諾登批 黑暗的一天 英國警方稍早證實，他們是在「厄瓜多政府撤銷庇護後，應厄國大使要求，進入館內逮捕艾山吉」，理由是違反2012年逮捕令。維基解密在艾山吉被捕後發推文批評，厄國以非法之舉中止政治庇護違反國際法，同為揭密者、目前流亡俄羅斯的美國前國安局雇員史諾登，則表示這是「新聞自由史上黑暗的一天」。 一心想攆走艾山吉的厄國總統莫雷諾昨天說，「厄方對艾山吉舉止的耐性已到極限，」且對方「多次違反國際公約和日常生活協議。」 厄總統稱 忍他到極限 維基解密因公開40萬件美國機密外交與軍事電文，讓美國與盟邦滿臉豆花，包括台灣在內的政治人物都被牽連其中，被稱為「外交界的911事件」，艾山吉因此成華府眼中釘，美國早磨刀霍霍意圖把他引渡至美報洩密之仇。 但導致艾山吉淪為通緝犯的源頭，是他2010年在瑞典涉嫌性侵與性騷擾，他離境後遭瑞典通緝而向英國自首。他為避免遭引渡至美國，乾脆違反保釋條款，在2012年6月進入厄國駐倫敦大使館尋求政治庇護並獲准。 厄瓜多為保護艾山吉不惜槓上英、美，但時日一久，他們的耐性逐漸消磨殆盡。莫雷諾還下令，撤除使館的額外保安人員，因為代號「訪客行動」( 或飯店行動)的艾山吉保護專案，每月耗資近200萬台幣。"
+    }
+
+}
+let vm = new Vue({
+    el: "#app",
+    data: data,
+    methods: {
+        goTop() {
+            document.documentElement.scrollTop = 0;
+        }
+    },
+    mounted: function() {
+        var mySwiper = new Swiper('.swiper-container', {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            on: {
+                slideChangeTransitionStart: function() {
+                    $('.back').css('opacity', 0.5);
+                    $('.swiper-slide-active').find('.back').css('opacity', 0);
+                }
+            },
+        })
+
+        var swiper2 = new Swiper('.select', {
+            width: 50,
+            spaceBetween: 0,
+            freeMode: true,
+            loop: false,
+            autoHeight: true,
+            navigation: {
+                nextEl: '.select-next',
+                prevEl: '.select-prev',
+            },
+        })
+    },
+
+
+
+})
